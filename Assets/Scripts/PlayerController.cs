0fd30c8748f48fcc23d36207ca30ed5cd10cf1df
Pirateboyfoxy <146957835+Pirateboyfoxy@users.noreply.github.com>
2024-05-23 09:22:27 -0400

    Project setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D playerRb;
     // Start is called before the first frame update
     void Start()
     {
+        playerRb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(new Vector2(0,1) * 5, ForceMode2D.Impulse);
+            Debug.Log("Space bar was pressed");
+        }
     }
 }
